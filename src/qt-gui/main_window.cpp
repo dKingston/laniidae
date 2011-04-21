ed63dab3f67dd86c30d3662d3374451aea1a986d
Michael Rodriguez <dkingston02@gmail.com>
2011-04-21 16:40:44 -0400

    Updated main window code

--- src/qt-gui/main_window.cpp
+++ src/qt-gui/main_window.cpp
@@ -29,6 +29,55 @@ MainWindow::MainWindow(void)
 MainWindow::~MainWindow(void)
 { }
 
+// CLASS METHOD: MainWindow::create_actions()
+// PURPOSE:      Create actions which are done by the end-user
+// ARGUMENTS:    None
+// RETURNS:      None
+void
+MainWindow::create_actions(void)
+{
+    mount_cd_image = new QAction(tr("Mount CD-ROM Image"), this);
+    connect(mount_cd_image, SIGNAL(triggered()), this, SLOT(mount_cdrom_image()));
+
+    mount_cd_disc = new QAction(tr("Mount CD-ROM Disc"), this);
+    connect(mount_cd_disc, SIGNAL(triggered()), this, SLOT(mount_cdrom_disc()));
+
+    umount_cd = new QAction(tr("Unmount CD-ROM"), this);
+    connect(umount_cd, SIGNAL(triggered()), this, SLOT(unmount_cdrom()));
+
+    conf = new QAction(tr("Configure"), this);
+    connect(conf, SIGNAL(triggered()), this, SLOT(configure()));
+
+    end = new QAction(tr("Exit"), this);
+    connect(end, SIGNAL(triggered()), this, SLOT(app_exit()));
+}
+
+// CLASS METHOD: MainWindow::create_menus()
+// PURPOSE:      Create menus for the end-user
+// ARGUMENTS:    None
+// RETURNS:      None
+void
+MainWindow::create_menus(void)
+{
+    file = menuBar()->addMenu(tr("&File"));
+    file->addAction(mount_cd_image);
+    file->addAction(mount_cd_disc);
+    file->addAction(umount_cd);
+    file->addSeparator();
+    file->addAction(conf);
+    file->addAction(end);
+}
+
+// CLASS METHOD: MainWindow::create_status_bar()
+// PURPOSE:      Create status bar for the end-user
+// ARGUMENTS:    None
+// RETURNS:      None
+void
+MainWindow::create_status_bar(void)
+{
+    statusBar()->showMessage(tr("Vi veri universum vivus vici"));
+}
+
 // CLASS METHOD: MainWindow::mount_cdrom_image()
 // PURPOSE:      Called when "File->Mount CD-ROM image" is clicked on by a user.
 // ARGUMENTS:    None
