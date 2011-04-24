64bf4b655c63107454fc4d68ef53d6007e65fc35
Michael Rodriguez <dkingston02@gmail.com>
2011-04-24 11:31:33 -0400

    Wizard header file

---
+++ src/qt-gui/wizard.hpp
@@ -0,0 +1,7 @@
+// qt-gui -- A Qt GUI interface to liblaniidae.
+// Copyright (c) 2011 Michael Rodriguez et al.
+// Rights to this code are documented in `docs/LICENSE`.
+
+#pragma once
+
+#include <QWizard>
